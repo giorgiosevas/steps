88b4a3782f4a4011906f9851105d0bd8efb6a3c2
George Sevastiadis <gsevasti@netcompany-intrasoft.com>
2023-12-09 19:04:09 +0200

    init the step project

---
+++ src/buttons.js
@@ -0,0 +1,38 @@
+// //Generic imports
+// import React, { useState } from 'react';
+// import Col from 'react-bootstrap/Col';
+// import Row from 'react-bootstrap/Row';
+// import Container from 'react-bootstrap/Container';
+// //CSS imports
+// import './index.css'
+
+// export const Buttons = () => {
+
+//     const [changeStep,setChangeStep] = useState()
+
+//     return(
+//         <Container fluid>
+//             <Row>
+//                 <button className='close'>
+//                     x
+//                 </button>
+//             </Row>
+// 			<Row className='buttons'>
+//                 <button disabled>1</button>
+//                 <button disabled>2</button>
+//                 <button disabled>3</button>
+//             </Row>
+//             <Row className='steps'>
+//                 <h3>
+//                     Step 1: Learn React ⚛
+//                     Step 2: Apply to jobs 💼
+//                     Step 3: Invest your new income 🤑
+//                 </h3>
+//             </Row>
+//             <Row className='buttons'>
+//                 <button>Previous</button>
+//                 <button>Next</button>
+//             </Row>
+// 		</Container>
+//     )
+// }
