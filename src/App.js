1b9202021a6c0c6fb19aa42e59ba3225c0d4a934
George Sevastiadis <gsevasti@netcompany-intrasoft.com>
2023-12-10 11:03:04 +0200

    init states+handle functions

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 //import { Buttons } from "./buttons"
 // import { OpenCloseButtons } from "./openCloseButton"
 //import './index.css'
@@ -9,11 +10,16 @@ const messages = [
 
 
 export default function App() {
+  const [step, setStep] = useState(1);
 
-  const step = 2;
+  //const step = 2;
 
   const handlePrevious = () => {
-    alert('ante geia')
+    if(step > 1) setStep(step - 1)
+  }
+
+  const handleNext = () => {
+    if(step < 3) setStep(step + 1)
   }
 
   return (
@@ -21,9 +27,9 @@ export default function App() {
       {/* <OpenCloseButtons/> */}
       {/* <Buttons/> */}
       <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
 
       <p className="message">Step {step}: {messages[step - 1]}</p>
@@ -35,7 +41,12 @@ export default function App() {
         >
           Previous
         </button>
-        <button style={{backgroundColor:'#7950f2', color:'#fff'}}>Next</button>
+        <button 
+          style={{backgroundColor:'#7950f2', color:'#fff'}} 
+          onClick={handleNext}
+        >
+          Next
+        </button>
       </div>
 
     </div>
